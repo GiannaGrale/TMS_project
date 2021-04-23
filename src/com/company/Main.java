e29697a629684b146e80b72d3d592cea3208dae3
Anna <giannagrale@gmail.com>
2021-04-23 13:37:57 +0300

    lesson_2_tasks

--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -3,7 +3,23 @@ package com.company;
 public class Main {
 
     public static void main(String[] args) {
-        System.out.println("Hello");
+
+        Lesson_2.task_1();
+        Lesson_2.task_2();
+        Lesson_2.task_3();
+        Lesson_2.task_4();
+        Lesson_2.task_5();
+        Lesson_2.task_6();
+        Lesson_2.task_7();
 
     }
 }
+
+
+
+
+
+
+
+
+
