3f7926bb561c1c19aef98ce7ffef4fa196b946e3
Anna <giannagrale@gmail.com>
2021-06-01 19:46:23 +0300

    shop menu updated

--- src/com/company/lesson_8/ShopMenu.java
+++ src/com/company/lesson_8/ShopMenu.java
@@ -15,6 +15,8 @@ public class ShopMenu {
         shop.addMerch(new Merch(5, "Jacket", 800));
         shop.addMerch(new Merch(6, "Socks", 100));
 
+        boolean active = true;
+        while(active) {
         System.out.println("Choose option:\n "
                 + "-------------------------\n"
                 + "1 - Display the merch\n"
@@ -26,68 +28,68 @@ public class ShopMenu {
         Scanner scanner = new Scanner(System.in);
         int choice = scanner.nextInt();
 
-        switch (choice) {
-            case 1:
-                shop.listMerch();
-                System.out.println("Insert 6 to choose prices in ascending order.\n"
-                        + "Insert 7 to choose prices in descending order.\n"
-                        + "Insert 8 to choose the latest items.");
-                choice = scanner.nextInt();
-                if (choice == 6) {
-                    shop.priceUpSort();
-
-                } else if (choice == 7) {
-                    shop.priceDownSort();
-
-                } else if (choice == 8) {
-                    shop.sortOrder();
-                }
-                this.start();
-                break;
-
-            case 2:
-                System.out.println("Insert the ID, name and price of the added item: ");
-                try {
-                    shop.addMerch(new Merch(scanner.nextInt(), scanner.next(), scanner.nextInt()));
+            switch (choice) {
+                case 1:
                     shop.listMerch();
-                } catch (Exception ex) {
-                    ex.printStackTrace();
-                    System.out.println("Input error. Try again!");
-                }
-                this.start();
-                break;
+                    System.out.println("Insert 6 to choose prices in ascending order.\n"
+                            + "Insert 7 to choose prices in descending order.\n"
+                            + "Insert 8 to choose the latest items.");
+                    choice = scanner.nextInt();
+                    if (choice == 6) {
+                        shop.priceUpSort();
 
-            case 3:
-                System.out.println("Insert the ID of the item to be deleted: ");
-                try {
-                    shop.deleteMerch(scanner.nextInt());
-                    shop.listMerch();
+                    } else if (choice == 7) {
+                        shop.priceDownSort();
 
-                } catch (Exception ex) {
-                    ex.printStackTrace();
-                    System.out.println("Input error. Try again!");
-                }
-                this.start();
-                break;
+                    } else if (choice == 8) {
+                        shop.sortOrder();
+                    }
+                    break;
 
-            case 4:
-                System.out.println("Insert the ID of the item to be edited: ");
-                try {
-                    shop.editMerch(new Merch(scanner.nextInt(), scanner.next(), scanner.nextInt()));
-                    shop.listMerch();
-                } catch (Exception e) {
-                    e.printStackTrace();
-                    System.out.println("Input error. Try again!");
-                }
-                start();
-                break;
+                case 2:
+                    System.out.println("Insert the ID, name and price of the added item: ");
+                    try {
+                        shop.addMerch(new Merch(scanner.nextInt(), scanner.next(), scanner.nextInt()));
+                        shop.listMerch();
+                    } catch (Exception ex) {
+                        ex.printStackTrace();
+                        System.out.println("Input error. Try again!");
+                    }
+                    break;
 
-            case 5:
-                System.out.println("Exit the system...");
-                break;
-            default:
-                System.out.println("Ops, something is wrong. Try again!");
-                break;
+                case 3:
+                    System.out.println("Insert the ID of the item to be deleted: ");
+                    try {
+                        shop.deleteMerch(scanner.nextInt());
+                        shop.listMerch();
+
+                    } catch (Exception ex) {
+                        ex.printStackTrace();
+                        System.out.println("Input error. Try again!");
+                    }
+                    break;
+
+                case 4:
+                    System.out.println("Insert the ID of the item to be edited: ");
+                    try {
+                        shop.editMerch(new Merch(scanner.nextInt(), scanner.next(), scanner.nextInt()));
+                        shop.listMerch();
+                    } catch (Exception e) {
+                        e.printStackTrace();
+                        System.out.println("Input error. Try again!");
+                    }
+                    break;
+
+                case 5:
+                    active = false;
+                    System.out.println("Exit the system...");
+                    break;
+
+                default:
+                    active = false;
+                    System.out.println("Ops, something is wrong. Try again!");
+                    break;
+            }
         }
     }
 }
